9ee29d70bb238a911071d51800da9bf5d2665c18
Stoian <stoian1929@mail.bg>
2018-03-20 18:15:06 +0200

    Add files via upload

---
+++ 2.MethodsAndDebugging/Exercises/5.FibonacciNumbers/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _5.FibonacciNumbers
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            Console.WriteLine(Fib(n));
+        }
+
+        static long Fib(long n)
+        {
+            long oneStepBehind = 1;
+            long currrentNumber = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                long tempLong = currrentNumber;
+                currrentNumber += oneStepBehind;
+                oneStepBehind = tempLong;
+            }
+            return currrentNumber;
+        }
+    }
+}
